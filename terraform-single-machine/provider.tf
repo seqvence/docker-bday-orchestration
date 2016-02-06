233615877d1b56cf5eca0a39ca55a4c8d254f0e4
alex giurgiu <alex.giurgiu@gmail.com>
2016-02-06 14:21:15 +0100

    creating a single machine for dev and testing

--- terraform-single-machine/provider.tf
+++ terraform-single-machine/provider.tf
@@ -1,7 +1,8 @@
 variable "do_token" {}
 variable "pub_key" {}
 variable "pvt_key" {}
-variable "ssh_fingerprint" {}
+variable "ssh_fingerprint_1" {}
+variable "ssh_fingerprint_2" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
